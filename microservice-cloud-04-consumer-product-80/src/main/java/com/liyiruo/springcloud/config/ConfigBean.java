8adc795c3eef2112eee3e755a3482ad6b83ce8d5
liyiruo <401213245@qq.com>
2020-07-05 23:22:46 +0800

    客户端负载均衡 Ribbon;1。将消费者模块添加到注册中心，2。在RestTemplate 上添加注解 @LoadBalanced，3。将请求地址换成eureka实例名称

--- microservice-cloud-04-consumer-product-80/src/main/java/com/liyiruo/springcloud/config/ConfigBean.java
+++ microservice-cloud-04-consumer-product-80/src/main/java/com/liyiruo/springcloud/config/ConfigBean.java
@@ -1,5 +1,6 @@
 package com.liyiruo.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,13 @@ import org.springframework.web.client.RestTemplate;
  */
 @Configuration
 public class ConfigBean {
+    /**
+     *  @LoadBalanced 表示这个RestTemplate开启负载均衡，
+     * 在调用服务提供者的接口时， 可使用 服务名称 替代真实IP地址。
+     * 服务名称 就是服务提供者在application.yml中
+     * 配置的spring.application.name属性的值。
+     */
+    @LoadBalanced
     @Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
