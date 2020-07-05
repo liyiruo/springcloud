8a18a63714ff730e5b48bb8768c51eda28704ab7
liyiruo <401213245@qq.com>
2020-07-05 17:48:51 +0800

    配置eureka 客户端，1.引入依赖 2.配置信息 3.添加注解

--- microservice-cloud-03-provider-product-8001/src/main/java/com/liyiruo/springcloud/ProductProvider_8001.java
+++ microservice-cloud-03-provider-product-8001/src/main/java/com/liyiruo/springcloud/ProductProvider_8001.java
@@ -3,9 +3,10 @@ package com.liyiruo.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 @MapperScan("com.liyiruo.springcloud.mapper")
 public class ProductProvider_8001 {
     public static void main(String[] args) {
